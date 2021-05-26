f345fd804f0bfe88d138316a36423c01cdd2b24c
peaceandwhisky <pioneerandf@gmail.com>
2021-05-27 04:29:44 +0900

    chapter4 p152

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -1,3 +1,27 @@
+use clap::Clap;
+
+#[derive(Clap,Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "peaceandwhisky",
+    about = "Super awesome sample RPN calcular"
+)]
+struct Opts {
+    /// Sets the level of verbosity
+    #[clap(short, long)]
+    verbose: bool,
+
+    /// Formulas written in RPN
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+    match opts.formula_file {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified."),
+    }
+    println!("Is verbosity specified?: {}", opts.verbose);
 }
