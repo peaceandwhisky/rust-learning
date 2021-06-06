11ef2d4c74cde314cb811f90051709fa64d65696
peaceandwhisky <pioneerandf@gmail.com>
2021-06-07 01:52:18 +0900

    p175

--- samplecli/src/bin/err_panic.rs
+++ samplecli/src/bin/err_panic.rs
@@ -1,36 +1,15 @@
-enum MyError {
-    Io(std::io::Error),
-    Num(std::num::ParseIntError),
-}
-
-use std::fmt
-
-impl fmt::Display for MyError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            MyError::Io(cause) => write!(f, "I/O Error: {}", cause),
-            MyError::Num(cause) => write!(f, "Parse Error: {}", cause),
-        }
-    }
-}
-
-fn get_int_from_file() -> Result<i32, MyError> {
+fn get_int_from_file() -> i32 {
     let path = "number.txt";
-    let num_str = std::fs::read_to_string(path).map_err(|e| MyError::Io(e))?;
 
-    num_str
+    let num_str = std::fs::read_to_string(path).expect("failed to open the file.");
+    let ret = num_str
         .trim()
         .parse::<i32>()
-        .map(|t| t * 2)
-        .map_err(|e| MyError::Num(e))
+        .expect("failed to parse string to a number.");
+    
+    ret * 2
 }
 
 fn main() {
-    match get_int_from_file() {
-        Ok(x) => println!("{}", x),
-        Err(e) => match e {
-            MyError::Io(cause) => println!("I/O Error: {}", cause),
-            MyError::Num(cause) => println!("Parse Error: {}", cause),
-        },
-    }
+    println!("{}", get_int_from_file());
 }
